8308df712917542fd2d302a65674a5eda401be26
simonmartyr <simonmartyr@hotmail.co.uk>
2013-02-05 14:49:00 +0000

    Nueral Network Layers and logic
    Layer creation adding layers. Addition the network processing input has
    been pre implmented.
    Note that weight range can only be between 1 and -1. sigmoid also in
    common functions. seems to work correclty

--- nueralNetwork.js
+++ nueralNetwork.js
@@ -2,29 +2,55 @@ var Nueral = (function(){
 	var nuerons = []; //array of all nuerons 
 	var layers 	= [];
 	var outputs = [];
+	var inputs 	= [];
+	var hiddenLayers = 0; //number of hidden layers
 	
 	function Nueral(){
 		
 	};
 	
-	Nueral.prototype.addNueron = function(inputs){ //data to be processed. 
+	/********General functions************/
+	
+	Nueral.prototype.reset = function(){ //new network restart
+		layers = [];
+		outputs = [];
+		nuerons = [];
+		inputs = [];
+		hiddenLayers = 0; 
+	}
+	
+	Nueral.prototype.getOutputs = function() { // return output
+		return outputs; 
+	};
+	
+	Nueral.prototype.run = function(inputs) { //expected 2 input array input
+		
+	};
+	
+	/********Nueron functions************/
+	
+	Nueral.prototype.addNueron = function(inputs, outputs){ //data to be processed. 
 		var nueronJSON = {"name"	 	: nuerons.length + 1,
 											"inputs" 	: inputs,
-											"weights" : inputs + 1,
-											"weightValues" : ""
+											"weights" : inputs,
+											"outputs" : outputs,
+											"weightValues" : "",
+											"threshold" : "",
+											"layer"		: false
 											}; //nueronObject
 			nuerons.push(nueronJSON);
 	};
 	
+	
 	Nueral.prototype.setWeight = function(weights, nueronName){ //expect array
-	  for (var i = 0; i < nuerons.length; i++){
-			if(nuerons[i]["name"] == nueronName){
-				var store = i;
-				break;
+	  var location = findNueron(nueronName);
+		if(location){
+			if(weights.length == nuerons[location]["weights"]){ // check right ammount of weights
+				nuerons[location]["weightValues"] = weights;
 			}
-		}
-		if(weights.length == nuerons[i]["weights"]){ // check right ammount of weights
-			nuerons[i]["weightValues"] = weights;
+			else{
+				return false; 
+			}
 		}
 		else{
 			return false; 
@@ -32,15 +58,101 @@ var Nueral = (function(){
 	};
 	
 	
-	Nueral.prototype.setWeights = function(data){ //take in GA created weights
-		weights = []; //reset weights
-		for(var i = 0; i < data.length; i++){
-			weights.push(data[i]);
+	Nueral.prototype.setThreshold = function(threshold, nueronName){ //set threshold of nueron
+		var location = findNueron(nueronName);
+		if(location){
+			nuerons[location]["threshold"] = threshold;
+		}
+		else{
+			return false; 
 		}
 	};
 	
-	Nueral.prototype.getOutputs = function() {
-		return outputs; 
+	
+	
+	/********Layer functions************/
+	
+	Nueral.prototype.addLayer = function(nueronLimit){ //create layer set nueron limit
+		var layerJSON = { "name" 				: layers.length + 1,
+											"nuerons" 		: [],
+											"nueronLimit" : nueronLimit,
+											"inputs"			: 0,    
+											"outputs" 		: 0,    
+										}; // layer object, inputs and outputs are running total.
+										
+		layers.push(layerJSON); // add layer to array
+	};
+	
+	Nueral.prototype.addNueronToLayer(name, layer){ //add nueron to layer
+		var layer = findLayer(layer);
+		var nueron = findNueron(name); 
+		if(layer != false && nueron != false){ //make sure that if both false doesn't fire
+			if(!nuerons[nueron]["layer"] && 
+				layers[layer]["nueronLimit"] != layers[layer]["nuerons"].length) //is not part of a layer
+			{
+				layers[layer]["nuerons"].push(name);
+				layers[layer]["inputs"] = layers[layer]["inputs"] + nuerons[nueron]["inputs"];
+				layers[layer]["outputs"] = layers[layer]["outputs"] + nuerons[nueron]["outputs"];
+				nuerons[nueron]["layer"] = true;
+			}
+		}
+		hiddenLayers++;
+	};
+	
+	
+	/********Running private functions************/
+	
+	function beginNetwork(inputs){ // expect array
+	  for(var i = 0; i < hiddenLayers; i++){
+			inputs = processLayers(inputs, i); //send inputs to a layer get outputs recycle 
+		}
+	};
+	
+	function processLayer(inputs, layerName){ // results from a layer  left to right = top to bottom... yeah!
+		var location = findLayer(layerName); //check exsistance
+		var outputs  = [];
+		for(var i = 0; i < layers[location]["nuerons"].length; i++)
+		{
+			var processed = processNueron(layers[location]["nuerons"][i], inputs);
+			for(var j = 0; j < processed.length; j++){
+				outputs.push(processed[j]);
+			}
+		}
+		return outputs;
+	};
+	
+	function processNueron(nueronName, inputs){ // da serious maths man
+		var theNueron = findNueron(nueronName);
+		var outputs = [];
+		for(var
+	};
+
+	
+	/********Common private functions************/
+	
+	function findNueron(name){ // take name return index 
+		for (var i = 0; i < nuerons.length; i++){
+			if(nuerons[i]["name"] == name){
+				var store = i;
+				return i;
+			}
+		}
+		return false; //not found
+	};
+	
+	function sigmoid(t) { // reference http://www.zacwitte.com/javascript-sigmoid-function
+    return 1/(1+Math.pow(Math.E, -t));
+	}
+
+	
+	function findLayer(name){ // take name return index 
+		for (var i = 0; i < layers.length; i++){
+			if(layers[i]["name"] == name){
+				var store = i;
+				return i;
+			}
+		}
+		return false; //not found
 	};
 
 
