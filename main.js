3b9d0e41662f4ee586fc2e43dd8314f2eb2fd182
simonmartyr <simonmartyr@hotmail.co.uk>
2013-02-26 15:54:57 +0000

    nueral network fixes
    working progress

--- main.js
+++ main.js
@@ -4,4 +4,30 @@ $(window).load(function()
 		plan = new Plan();
 		ga = new Ga();
 		enviroment = new Enviroment();
+		nueralNetwork = new Nueral();
 	});
+	
+	
+	function setupNueralNetwork()
+	{
+		nueralNetwork.addLayer(3); //1st layer
+		nueralNetwork.addLayer(2); //2nd layer
+		
+		/*** layer 1 ***/
+		nueralNetwork.addNueron(1 , 2); 
+		nueralNetwork.addNueron(1 , 2);
+		nueralNetwork.addNueron(1 , 2);
+		
+		nueralNetwork.addNueronToLayer(1 , 1);
+		nueralNetwork.addNueronToLayer(2 , 1);
+		nueralNetwork.addNueronToLayer(3 , 1);
+		
+		
+		/*** layer 2 ***/
+		
+		nueralNetwork.addNueron(3 , 1); 
+		nueralNetwork.addNueron(3 , 1);
+		
+		nueralNetwork.addNueronToLayer(4 , 2);
+		nueralNetwork.addNueronToLayer(5 , 2);
+	}
--- nueralNetwork.js
+++ nueralNetwork.js
@@ -19,33 +19,54 @@ var Nueral = (function(){
 		hiddenLayers = 0; 
 	}
 	
-	Nueral.prototype.getOutputs = function() { // return output
+	Nueral.prototype.getOutputs = function() { // return output 2 01 00 etc
 		return outputs; 
 	};
 	
-	Nueral.prototype.run = function(inputs) { //expected 2 input array input
+	Nueral.prototype.run = function(inputs) { //expected 3 input array input
 		
+	};
+	
+	Nueral.prototype.getAll = function(){
+		console.log(nuerons);
+		console.log(layers);
+		console.log(hiddenLayers);
 	};
 	
 	/********Nueron functions************/
 	
 	Nueral.prototype.addNueron = function(inputs, outputs){ //data to be processed. 
-		var nueronJSON = {"name"	 	: nuerons.length + 1,
-											"inputs" 	: inputs,
-											"weights" : inputs,
-											"outputs" : outputs,
-											"weightValues" : "",
-											"threshold" : "",
-											"connectedTo" : [],
-											"layer"		: false
+		var nueronJSON = {"name"	 	: nuerons.length + 1,  //1 - *
+											"inputs" 	: inputs, //number of 
+											"weights" : inputs, //number of
+											"outputs" : outputs, //number of
+											"weightValues" : "",  //value of each connection
+											"threshold" : "", //sig? 
+											"connectedTo" : [],  //which nueron to send data. 
+											"layer"		: false //nueron in a layer
 											}; //nueronObject
-			nuerons.push(nueronJSON);
+			nuerons.push(nueronJSON); //all nuerons
 	};
 	
 	
 	Nueral.prototype.setWeight = function(weights, nueronName){ //expect array
 	  var location = findNueron(nueronName);
-		if(location){
+		if(!isNaN(location)){
+			if(weights.length == nuerons[location]["weights"]){ // check right ammount of weights
+				nuerons[location]["weightValues"] = weights;
+			}
+			else{
+				return false; 
+			}
+		}
+		else{
+			return false; 
+		}
+	};
+	
+	Nueral.prototype.connectTo = function(nueronName, connections){
+		var location = findNueron(nueronName);
+		if(!isNaN(location)){
 			if(weights.length == nuerons[location]["weights"]){ // check right ammount of weights
 				nuerons[location]["weightValues"] = weights;
 			}
@@ -82,12 +103,13 @@ var Nueral = (function(){
 										}; // layer object, inputs and outputs are running total.
 										
 		layers.push(layerJSON); // add layer to array
+		hiddenLayers++;
 	};
 	
-	Nueral.prototype.addNueronToLayer(name, layer){ //add nueron to layer
+	Nueral.prototype.addNueronToLayer = function(name, layer){ //add nueron to layer
 		var layer = findLayer(layer);
 		var nueron = findNueron(name); 
-		if(layer != false && nueron != false){ //make sure that if both false doesn't fire
+		if(!isNaN(layer) && !isNaN(nueron)){ //make sure that if both false doesn't fire
 			if(!nuerons[nueron]["layer"] && 
 				layers[layer]["nueronLimit"] != layers[layer]["nuerons"].length) //is not part of a layer
 			{
@@ -97,7 +119,6 @@ var Nueral = (function(){
 				nuerons[nueron]["layer"] = true;
 			}
 		}
-		hiddenLayers++;
 	};
 	
 	
@@ -125,7 +146,7 @@ var Nueral = (function(){
 	function processNueron(nueronName, inputs){ // da serious maths man
 		var theNueron = findNueron(nueronName);
 		var outputs = [];
-		for(var
+		
 	};
 
 	
